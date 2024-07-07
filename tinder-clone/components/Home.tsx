6245de21979211de81ae4a74889b18b6b7ed9074
HirenGajjar <gajjarhiren111@gmail.com>
2024-07-06 23:16:12 -0700

    Add card component

--- tinder-clone/components/Home.tsx
+++ tinder-clone/components/Home.tsx
@@ -1,17 +1,54 @@
 "use client";
 import { Neo4juser } from "@/types";
 import * as React from "react";
+import TinderCard from "react-tinder-card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 
 interface HomePageClientProps {
   currentUser: Neo4juser;
   users: Neo4juser[];
 }
 
+const handleSwipe = (direction: string, userId: string) => {};
+
 const HomePageClient: React.FC<HomePageClientProps> = ({
   currentUser,
   users,
 }) => {
-  return <></>;
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center mb-4">
+        <div>
+          <h1 className="text-4xl ">Hello, {currentUser.firstname}</h1>
+        </div>
+        <div className="relative">
+          {users.map((user) => (
+            <TinderCard
+              className="absolute"
+              onSwipe={(direction) =>
+                handleSwipe(direction, user.applicationId)
+              }
+              key={user.applicationId}
+            >
+              <Card>
+                <CardHeader>
+                  <CardTitle>{user.firstname}</CardTitle>
+                  <CardDescription>{user.email}</CardDescription>
+                </CardHeader>
+              </Card>
+            </TinderCard>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePageClient;
