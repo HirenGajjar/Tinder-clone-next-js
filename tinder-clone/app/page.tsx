0a0e74772d5d3fb1fd38b8a63402c2122696b411
HirenGajjar <gajjarhiren111@gmail.com>
2024-07-06 20:14:44 -0700

    Add user logged signup in check

--- tinder-clone/app/page.tsx
+++ tinder-clone/app/page.tsx
@@ -1,5 +1,21 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <main></main>;
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+export default async function Home() {
+  // Check if user is logged in or not
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  // If not then redirect to this page, basically a login or sign up page
+  if (!(await isAuthenticated())) {
+    return redirect(
+      "/api/auth/login?post_login_redirect_url=http://localhost:3000/callback"
+    );
+  }
+  // Check is there is no user
+  const user = await getUser();
+  // If not then redirect to this page, basically a login or sign up page
+  if (!user) {
+    return redirect(
+      "/api/auth/login?post_login_redirect_url=http://localhost:3000/callback"
+    );
+  }
+  return <main>Hii {user.given_name}</main>;
 }
